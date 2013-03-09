728429d54aea74458f5de6267c02fcddcdab224f
Pepe Cano <ppcanodehuelva@gmail.com>
2013-03-09 20:40:25 +0100

    Add filter for android import

--- rakep/filters.rb
+++ rakep/filters.rb
@@ -6,6 +6,7 @@ module Yoin
 
       super(options, context, &block)
       @options = { :paths => ["#{Yoin.submodule_path}yoin-views-android/less/import", 
+                              "#{Yoin.submodule_path}yoin-views/less/import-android", 
                               "#{Yoin.submodule_path}yoin-views/less/import", 
                               "#{Yoin.submodule_path}yoin-views/less/import2"] }
     end
